f1cfdc77a9707a2d89c0a783d6d28907fcbbfcef
Marcelo Hexsel <mhexsel@umov.me>
2019-07-24 20:39:00 -0300

    currentDate component

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <current-date />
+  </div>
+</template>
+
+<script>
+import lib from "lib";
+
+export default {
+  name: "app",
+  components: {
+    CurrentDate: lib.CurrentDate
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lib/src/components/CurrentDate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="current-date">Now is {{currentDate}}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentDate: new Date()
+    };
+  }
+};
+</script>
+
+<style>
+.current-date {
+  color: red;
+}
+</style>
